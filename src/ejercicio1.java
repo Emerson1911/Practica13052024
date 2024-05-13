a00476f2b636482c382a015ae68e8741a3c51530
Emerson1911 <1284662@clases.edu.sv>
2024-05-13 11:39:27 -0600

    Commit de ejercicio1

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -3,22 +3,34 @@ import java.util.Scanner;
 public class ejercicio1 {
 
     public static void main(String[] args) throws Exception {
-       Scanner teclado = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
        short num ;
        short par = 0;
        short impar = 0;
 
-       for (int i = 0; i < 6; i++){
 
-        System.out.print("Ingresa el numero");
-        num = teclado.nextShort();
+      for (int i = 0; i < 6; i++){
 
-        if (num%2 == 0){
-            System.out.print("El numero es par " + num + " ");
-            i++;
-            par++;
+          System.out.print("Ingresa el numero");
+          num = teclado.nextShort();
+
+          if (num%2 == 0){
+              System.out.print("El numero es par " + num + " ");
+              i++;
+              par++;
+          }
+
+          else {
+              System.out.print("El numero es impar " + num + " ");
+              i++;
+              impar++;
+          }
+
         }
-    }
+
+      System.out.println("Total de pares " + par );
+        System.out.println("Total de impares " + impar );
+
 
 }
 }
