82c45f6cd20697416f8205acb7fe0b23162c2a76
German Steven Méndez Clavel <clavelsteven441@gmail.com>
2024-05-13 11:34:44 -0600

    for

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -8,5 +8,17 @@ public class ejercicio1 {
        short par = 0;
        short impar = 0;
 
+       for (int i = 0; i < 6; i++){
+
+        System.out.print("Ingresa el numero");
+        num = teclado.nextShort();
+
+        if (num%2 == 0){
+            System.out.print("El numero es par " + num + " ");
+            i++;
+            par++;
+        }
     }
+
+}
 }
